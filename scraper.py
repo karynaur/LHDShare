c4a2e241c277dc359a2ba01372050a3319fb490f
ADITYA SRINIVAS MENON <adityasrinivas.menon@gmail.com>
2021-04-03 19:07:26 +0530

    extentiosn

---
+++ scraper.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://localhackday.mlh.io/local-hack-day-share-challenges'
+r = requests.get(url)
+soup = BeautifulSoup(r.text, "html.parser")
+
+f=open('MLH.txt','a')
+mydivs = soup.find_all("h3", "hero-sub-head event")
+for i in mydivs:
+   for j in i:
+      f.write(j+'\n')
+f.close()
+
